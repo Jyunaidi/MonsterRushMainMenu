7610c44ad3f04f6b214ed33dee6dbf072bace8d7
Jyunaidi <nabil_24003319@utp.edu.my>
2024-10-05 16:46:34 +0800

    main menu

---
+++ Monster Rush MainMenu/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using swarmgame2;
+
+
+namespace Mswarmgame2
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call the GameManager class method to start the game
+            GameManager.StartGame();
+        }
+    }
+}
